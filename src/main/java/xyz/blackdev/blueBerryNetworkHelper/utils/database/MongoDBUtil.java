81feae623764a4148cc6c188fbbb899da6900b8c
BlackDev <BlackDevReal@proton.me>
2025-08-27 21:54:50 +0200

    added util

---
+++ src/main/java/xyz/blackdev/blueBerryNetworkHelper/utils/database/MongoDBUtil.java
@@ -0,0 +1,25 @@
+package xyz.blackdev.mongoHelper.utility;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+
+public class MongoDB {
+    public static void insertDocument(String uri, String dbName, String collectionName, Document doc) {
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase(dbName);
+            MongoCollection<Document> collection = database.getCollection(collectionName);
+            collection.insertOne(doc);
+        }
+    }
+
+    public static FindIterable<Document> findDocuments(String uri, String dbName, String collectionName) {
+        MongoClient mongoClient = MongoClients.create(uri);
+        MongoDatabase database = mongoClient.getDatabase(dbName);
+        MongoCollection<Document> collection = database.getCollection(collectionName);
+        return collection.find();
+    }
+}
